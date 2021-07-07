fa0da8d0ee741586159ed6d2b0fb84799bf901b8
Tatiana Iavoruk <61006466+Tatiana655@users.noreply.github.com>
2021-07-07 15:04:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import VisualParafac as vp
 
 if __name__ == '__main__':
     print("Hello!")
+    # NB: в папке like 1701.txt файлы с одинаковыми шагами по осям
     default_way = "C:\\Users\\Tatiana\\Desktop\\ToBazhenov\\VD_DOM_Permafrost\\"
     data = vp.read_tensor(default_way)
     data = vp.erase_Reyleigh(data,25//2,40//2)
     # Уточнить ширину линии в срезах по оси ox
-    #data = erase_Reyleigh_Raman(data,25//2,40//2, 36//2,36//2)
+    # data = erase_Reyleigh_Raman(data,25//2,40//2, 36//2,36//2)
     vp.show_data(data,3,5,"DATA",0,14)
     vp.show_components(data,2,2,4)
     vp.show_loadings(data,4)
